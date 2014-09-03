95dbb03a55efcf5c023e2f5c0a2fd4a15460f487
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-09-03 18:33:38 -0400

    added regex for Email's email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  #defines the VALID_EMAIL_REGEX variable for the below validation
+
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+   uniqueness: { case_sensitive: false }
+  #makes sure the user email is present, matches the exmaple@mail.com 
+  #(VALID_EMAIL_REGEX method), and gives the email uniqueness by make it 
+  #non-casesensitive.
 end
