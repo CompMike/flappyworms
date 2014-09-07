13de475b6dcdcd13d36bd3f74f9d21dbdf0d540c
Michael Heard <compmike@gmail.com>
2014-09-06 20:37:46 -0400

    removed username from repository replaced with ENV variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
     address: "smtp.mandrillapp.com",
     port: 587,
     authentication: "plain",
-    user_name: "compmike@gmail.com",
+    user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     enable_starttls_auto: false
   }
